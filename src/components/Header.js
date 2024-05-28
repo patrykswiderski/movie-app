e291a5b9f73ce9d9666d6ed4f065c5852c64ed89
Patryk <patryk.swiderski.85@gmail.com>
2024-05-28 20:44:32 +0200

    Mobile nav added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,28 @@
 import React, { useEffect, useState } from "react";
 import Logo from "../assets/logo_l.png";
-import { NavLink, useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import UserIcon from "../assets/user_icon.png";
 import { IoSearchOutline } from "react-icons/io5";
+import { navigation } from "../constants/navigation";
 
 const Header = () => {
   const [searchInput, setSearchInput] = useState("");
   const navigate = useNavigate();
 
-  const navigation = [
-    {
-      label: "TV shows",
-      href: "tv",
-    },
-    {
-      label: "Movies",
-      href: "movie",
-    },
-  ];
-
   useEffect(() => {
     navigate(`/search?q=${searchInput}`);
   }, [searchInput]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <header className="fixed top-0 w-full h-16 bg-neutral-600 bg-opacity-75">
       <div className="container mx-auto px-3 flex items-center h-full">
-        <div>
+        <Link to="/">
           <img src={Logo} alt="logo" width={120} />
-        </div>
+        </Link>
 
         <nav className="hidden lg:flex items-center gap-2 ml-5">
           {navigation.map((nav, index) => {
@@ -38,7 +32,7 @@ const Header = () => {
                   key={nav.label}
                   to={nav.href}
                   className={({ isActive }) =>
-                    `px-2 hover:text-neutral-50 ${isActive && " text-neutral"}`
+                    `px-2 hover:text-neutral-50 ${isActive && " text-white"}`
                   }
                 >
                   {nav.label}
@@ -49,11 +43,11 @@ const Header = () => {
         </nav>
 
         <div className="ml-auto flex items-center gap-4">
-          <form className="flex items-center gap-2">
+          <form className="flex items-center gap-2" onSubmit={handleSubmit}>
             <input
               type="text"
               placeholder="Search here..."
-              className="bg-transparent px-4 py-1 outline-none border-none"
+              className="bg-transparent px-4 py-1 outline-none border-none hidden lg:block"
               onChange={(e) => setSearchInput(e.target.value)}
               value={searchInput}
             />
---
+++ src/constants/navigation.js
@@ -0,0 +1,31 @@
+import { FaHome } from "react-icons/fa";
+import { PiTelevisionFill } from "react-icons/pi";
+import { BiSolidMoviePlay } from "react-icons/bi";
+import { IoSearchOutline } from "react-icons/io5";
+
+export const navigation = [
+  {
+    label: "TV shows",
+    href: "tv",
+    icon: <PiTelevisionFill />,
+  },
+  {
+    label: "Movies",
+    href: "movie",
+    icon: <BiSolidMoviePlay />,
+  },
+];
+
+export const mobileNavigation = [
+  {
+    label: "Home",
+    href: "/",
+    icon: <FaHome />,
+  },
+  ...navigation,
+  {
+    label: "Search",
+    href: "/search",
+    icon: <IoSearchOutline />,
+  },
+];
