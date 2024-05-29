6659d2f32751dde7e0ecae7b4cc436350cdc1c2c
Patryk <patryk.swiderski.85@gmail.com>
2024-05-29 17:22:28 +0200

    BannerHome upgrade

--- src/components/BannerHome.js
+++ src/components/BannerHome.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { IoPlayCircleSharp } from "react-icons/io5";
 
 const BannerHome = () => {
   const bannerData = useSelector((state) => state.movieflixData.bannerData);
@@ -9,7 +10,7 @@ const BannerHome = () => {
 
   return (
     <section className="w-full h-full">
-      <div className="flex min-h-full max-h-[95vh]">
+      <div className="flex min-h-full max-h-[95vh] overflow-hidden">
         {bannerData.map((data, index) => {
           return (
             <div
@@ -24,16 +25,27 @@ const BannerHome = () => {
                 />
               </div>
 
+{}
+
               <div className="absolute top-0 w-full h-full bg-gradient-to-t from-neutral-900 to-transparent"></div>
 
-              <div className="container mx-auto absolute bottom-0 max-w-md px-3">
-                <h2 className="font-bold text-2xl lg:text-4xl text-white drop-shadow-3xl">
-                  {data.title ?? data.name}
-                </h2>
-                <p className="text-ellipsis line-clamp-3 my-2">
-                  {data.overview}
-                </p>
-                <div></div>
+              <div className="container mx-auto">
+                <div className="w-full absolute bottom-0 max-w-md px-3">
+                  <h2 className="font-bold text-2xl lg:text-4xl text-white drop-shadow-3xl">
+                    {data.title ?? data.name}
+                  </h2>
+                  <p className="text-ellipsis line-clamp-3 my-2">
+                    {data.overview}
+                  </p>
+                  <div className="flex items-center gap-4">
+                    <p>Rating: {Number(data.vote_average).toFixed(1)}</p>
+                    <span>|</span>
+                    <p>View: {Number(data.popularity).toFixed(0)}</p>
+                  </div>
+                  <button className="flex items-center gap-2 bg-white hover:bg-gradient-to-l from-red-700 to-orange-500 px-3 py-2 text-black font-bold rounded mt-4 shadow-md transition-all hover:scale-105">
+                    Play Now <IoPlayCircleSharp className="text-2xl" />
+                  </button>
+                </div>
               </div>
             </div>
           );
